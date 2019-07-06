13efca719e4fd38af380fb53ee14fcc712d3a6d3
master <>
2019-07-07 01:11:08 +0900

    implement testTemp()

--- JSONParsingTests/JSONParsingTests.swift
+++ JSONParsingTests/JSONParsingTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import JSONParsing
 
 class JSONParsingTests: XCTestCase {
 
@@ -18,16 +19,10 @@ class JSONParsingTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testTemp() {
+        let absoluteTemperature:Float = 298.0
+        let viewController = ViewController()
+        let temp = viewController.getCelsius(temp: absoluteTemperature)
+        XCTAssertEqual(temp, Int(absoluteTemperature - 273.15))
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
