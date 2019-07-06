71c30826857fdae7b5976dbb95014140d3493ad8
master <>
2019-07-07 00:01:17 +0900

    add unit test

---
+++ JSONParsingTests/JSONParsingTests.swift
@@ -0,0 +1,33 @@
+//
+//  JSONParsingTests.swift
+//  JSONParsingTests
+//
+//  Created by 조전형 on 2019/07/07.
+//  Copyright © 2019 cho. All rights reserved.
+//
+
+import XCTest
+
+class JSONParsingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
